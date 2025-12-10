39a5b3cce3a7c99818b5e95b13b39808a2e67ba2
Siddharth Khamithkar <sidkhamithkar@gmail.com>
2025-12-10 16:02:36 +0530

    removed cache files

--- app/fastserver/source/botbackend.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from models import UserCreateRequest, UserCreateResponse
-app = FastAPI()
-
-
-@app.post("/echo")
-def echo_name(payload: UserCreateRequest):
-    return {"name": payload.name}
-
